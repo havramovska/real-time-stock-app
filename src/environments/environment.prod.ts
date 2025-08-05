8b411b64c965b2e44521f17e4c88e57297d2967e
hhristovska <152978408+hhristovska@users.noreply.github.com>
2025-08-05 21:59:45 +0200

    add app environments for dev and prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  useMockData: false,
+  apiUrl: 'https://finnhub.io/docs/api/introduction',
+  websocketUrl: '',
+  updateInterval: 5000
+}; 
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  useMockData: true,
+  apiUrl: '',
+  websocketUrl: '',
+  updateInterval: 3000
+}; 
